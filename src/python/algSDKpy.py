a4ff9a92823e3586f614629217b42125762f2974
jimmy <jimmy@ailiteam.com>
2022-06-12 16:42:00 +0800

    update python interface

---
+++ src/python/algSDKpy.py
@@ -0,0 +1,35 @@
+import os
+import ctypes
+from ctypes import *
+
+ALG_SDK_SERVICE_SENSOR_CONFIG_MAX_LINE = 1024
+ALG_SDK_MAX_CHANNEL = 32
+
+class service_camera_config(Structure):
+    _fields_ = [("ack_mode",c_uint8),
+    ("ch_id",c_uint8),
+    ("module_type", c_uint16),
+    ("width", c_uint16),
+    ("height", c_uint16),
+    ("line_len", c_uint16),
+    ("payload", c_uint8*7*ALG_SDK_SERVICE_SENSOR_CONFIG_MAX_LINE),
+    ("ack_code", c_uint8),
+    ("channel", c_uint8)
+    ]
+
+class service_stream_control(Structure):
+    _fields_ = [("ack_mode",c_uint8),
+    ("select",c_uint8*ALG_SDK_MAX_CHANNEL),
+    ("control", c_uint8*ALG_SDK_MAX_CHANNEL),
+    ("ack_code", c_uint8),
+    ("ch_sel", c_uint8*ALG_SDK_MAX_CHANNEL)
+    ]
+
+def CallServices(topic_ptr, cfg_ptr, timeo):
+    pcie_sdk = ctypes.CDLL('../../lib/linux/libpcie_sdk.so')
+    pcie_sdk.alg_sdk_call_service.argtypes = [c_char_p, c_void_p, c_int]
+    pcie_sdk.alg_sdk_call_service.restype = ctypes.c_int
+
+    ret = pcie_sdk.alg_sdk_call_service(c_char_p(topic_ptr), pointer(cfg_ptr), timeo)
+
+    return ret
---
+++ src/python/stream_on.py
@@ -0,0 +1,26 @@
+import algSDKpy
+
+cmd_topic = b"/service/camera/stream_on"
+timeo = 5000
+
+if __name__ == '__main__':
+    cam_ctl = algSDKpy.service_stream_control()
+    cam_ctl.ack_mode = 1
+
+    for i in range(0, 7):
+        cam_ctl.select[i] = 0
+        cam_ctl.control[i] = 0
+
+    cam_ctl.select[0] = 1
+    cam_ctl.control[0] = 0
+    cam_ctl.select[2] = 1
+    cam_ctl.control[2] = 1
+    cam_ctl.select[4] = 1
+    cam_ctl.control[4] = 0
+    cam_ctl.select[6] = 1
+    cam_ctl.control[6] = 0
+
+    ret = algSDKpy.CallServices(cmd_topic, cam_ctl, timeo)
+    print(' result = %d ' % ret)
+
+    print('---------finish-------------')
