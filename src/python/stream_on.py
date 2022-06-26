fb8eda2866d487862c2d0e0cf08ee084649dbe65
jimmy <jimmy@ailiteam.com>
2022-06-26 19:47:33 +0800

    Test SDK V0.4.7 OK

--- src/python/stream_on.py
+++ src/python/stream_on.py
@@ -13,15 +13,15 @@ if __name__ == '__main__':
         cam_ctl.control[i] = 0
 
     cam_ctl.select[0] = 1
-    cam_ctl.control[0] = 1
+    cam_ctl.control[0] = 0
     cam_ctl.select[1] = 1
     cam_ctl.control[1] = 0
     cam_ctl.select[2] = 1
-    cam_ctl.control[2] = 0
+    cam_ctl.control[2] = 1
     cam_ctl.select[3] = 1
     cam_ctl.control[3] = 0
     cam_ctl.select[4] = 1
-    cam_ctl.control[4] = 0
+    cam_ctl.control[4] = 1
     cam_ctl.select[5] = 1
     cam_ctl.control[5] = 0
     cam_ctl.select[6] = 1
