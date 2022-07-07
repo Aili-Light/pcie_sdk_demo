e22baabe3ce47831af849e1145f3ff0676112fe1
jimmy <jimmy@ailiteam.com>
2022-07-07 17:31:33 +0800

    Add image save in python

--- src/python/subscribe.py
+++ src/python/subscribe.py
@@ -13,37 +13,43 @@ from algSDKpy import callbackFunc_t
 client = algSDKClient()
 topic = b"/image_data/stream"
 
-def array2mat(w, h, payload, image_name, type):
+def array2mat(w, h, payload, image_name, ch_id, frame_index, type):
     p_array = np.frombuffer(np.ctypeslib.as_array(payload, shape=((h*w*2, 1, 1))), dtype=np.uint8)
-    is_save = 0
     
     if (type == 0):
         img_in = p_array.reshape((h, w, 2))
         img_out = cv2.cvtColor(img_in, cv2.COLOR_YUV2BGR_YUYV)
         img_disp = cv2.resize(img_out, (640, 360))
         cv2.imshow(str(image_name), img_disp)
-        if (is_save == 1):
-            cv2.imsave("test.jpg", img_disp)
         
-        cv2.waitKey(1)
+        filename = str("image_")+str("%02d_" % ch_id)+str("%08d_" % frame_index)+str(".bmp")
+        c = cv2.waitKey(1)
+        if c == 32:
+            cv2.imwrite(filename, img_out)
+
     elif (type == 1):
-        p_data = np.zeros(shape=(h*w*2, 1, 1), dtype=np.uint8)
+        p_data = np.zeros(shape=(h*w, 1, 1), dtype=np.uint16)
         for i in range(0, int(h*w/4)):
-            # print(np.ushort(p_array[5*i]))
             p_data[4*i] = (((np.ushort(p_array[5*i]) << 2) & 0x03FC) | np.ushort((p_array[5*i+4] >> 0) & 0x0003));
             p_data[4*i+1] = (((np.ushort(p_array[5*i+1]) << 2) & 0x03FC) | np.ushort((p_array[5*i+4] >> 2) & 0x0003));
             p_data[4*i+2] = (((np.ushort(p_array[5*i+2]) << 2) & 0x03FC) | np.ushort((p_array[5*i+4] >> 4) & 0x0003));
             p_data[4*i+3] = (((np.ushort(p_array[5*i+3]) << 2) & 0x03FC) | np.ushort((p_array[5*i+4] >> 6) & 0x0003));
         
-        if (is_save == 1):
-            np.save("test.raw", p_data)
-            print("saved image")
+        # filename = str("image_")+str("%02d_" % ch_id)+str("%08d_" % frame_index)+str(".raw")
+        # np.save(filename, p_data)
+
+def get_channel_id(topic_name):
+    topic = str(topic_name)
+    tx = topic.split("/")
+    ch_id = int(tx[3])
+    
+    return ch_id
 
 def CallbackFunc(ptr):
     p = ctypes.cast(ptr, ctypes.POINTER(pcie_image_data_t))
     payload = ctypes.cast(p.contents.payload, ctypes.POINTER(c_uint8))
     # print('[frame = %d], [time %ld], [byte_0 = %d], [byte_end = %d]' % (p.contents.image_info_meta.frame_index, p.contents.image_info_meta.img_size, payload[0], payload[p.contents.image_info_meta.img_size-1]))
-    array2mat(p.contents.image_info_meta.width, p.contents.image_info_meta.height, payload, p.contents.common_head.topic_name, 0)
+    array2mat(p.contents.image_info_meta.width, p.contents.image_info_meta.height, payload, p.contents.common_head.topic_name, get_channel_id(p.contents.common_head.topic_name), p.contents.image_info_meta.frame_index, 0)
 
 if __name__ == '__main__':
     callback_func = callbackFunc_t(CallbackFunc)
