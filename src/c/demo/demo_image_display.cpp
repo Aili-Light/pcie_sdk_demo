38b85a594cf7dc5f88d1fbc91c9f7df7c5ae94f9
jimmy.xie <jimmy-ASUS-ubuntu18@ailiteam.com>
2023-09-27 11:09:17 +0800

    add isp awb

--- src/c/demo/demo_image_display.cpp
+++ src/c/demo/demo_image_display.cpp
@@ -14,6 +14,7 @@
 #include "opencv2/highgui.hpp"
 #include <fstream>
 #include "alg_cvt/alg_cvtColor.h"
+#include "alg_isp/alg_isp_pipeline.h"
 
 using namespace std;
 const char topic_image_head_d[ALG_SDK_HEAD_COMMON_TOPIC_NAME_LEN] = {ALG_SDK_TOPIC_NAME_IMAGE_DATA};
@@ -27,10 +28,10 @@ static uint32_t g_f_last[ALG_SDK_MAX_CHANNEL] = {0};
 
 /*  Return the UNIX time in milliseconds.  You'll need a working
     gettimeofday(), so this won't work on Windows.  */
-uint64_t milliseconds (void)
+uint64_t milliseconds(void)
 {
     struct timeval tv;
-    gettimeofday (&tv, NULL);
+    gettimeofday(&tv, NULL);
     return (((uint64_t)tv.tv_sec * 1000) + ((uint64_t)tv.tv_usec / 1000));
 }
 
@@ -43,53 +44,53 @@ void int_handler(int sig)
     exit(sig);
 }
 
-void array_2_mat(uchar* data, int w, int h, int data_type, int ch_id, uint32_t frame_index, const char* image_name)
+void array_2_mat(uchar *data, int w, int h, int data_type, int ch_id, uint32_t frame_index, const char *image_name)
 {
     const uint32_t data_size = w * h;
     cv::Mat img_rgb8 = cv::Mat(h, w, CV_8UC3);
-    if(data_type <= ALG_SDK_MIPI_DATA_TYPE_YVYU)
+    if (data_type <= ALG_SDK_MIPI_DATA_TYPE_YVYU)
     {
         /* Image Display */
-        uchar* buf_rgb = img_rgb8.data;
+        uchar *buf_rgb = img_rgb8.data;
         /* yuv to rgb conversion */
         // cv::Mat img = cv::Mat(h, w, CV_8UC2, data);
         // cv::cvtColor(img, img_rgb8, cv::COLOR_YUV2BGR_YUYV);
-        alg_cv::alg_sdk_cvtColor((uchar*)data, (uchar*)buf_rgb, w, h, alg_cv::ALG_CV_YUV2RGBCVT_YUYV);
+        alg_cv::alg_sdk_cvtColor((uchar *)data, (uchar *)buf_rgb, w, h, alg_cv::ALG_CV_YUV2RGBCVT_YUYV);
         /* Image Display */
-//        cv::resize(img_rgb8, img_rgb8, cv::Size(1280,720));
+        //        cv::resize(img_rgb8, img_rgb8, cv::Size(1280,720));
         cv::namedWindow(image_name, CV_WINDOW_NORMAL);
         cv::imshow(image_name, img_rgb8);
     }
-    else if(data_type == ALG_SDK_MIPI_DATA_TYPE_RAW10)
+    else if (data_type == ALG_SDK_MIPI_DATA_TYPE_RAW10)
     {
         /* raw to rgb conversion */
         /* PCIE RAW Data Conversion */
-        uchar* pdata = (uchar*)malloc(sizeof(uchar) * data_size);
-        for(int i = 0; i < int(data_size / 4); i++)
+        uchar *pdata = (uchar *)malloc(sizeof(uchar) * data_size);
+        for (int i = 0; i < int(data_size / 4); i++)
         {
-            pdata[4*i] = ((((data[5*i]) << 2) & 0x03FC) | ((data[5*i+4] >> 0) & 0x0003)) >> 2;
-            pdata[4*i+1] = ((((data[5*i+1]) << 2) & 0x03FC) | ((data[5*i+4] >> 2) & 0x0003)) >> 2;
-            pdata[4*i+2] = ((((data[5*i+2]) << 2) & 0x03FC) | ((data[5*i+4] >> 4) & 0x0003)) >> 2;
-            pdata[4*i+3] = ((((data[5*i+3]) << 2) & 0x03FC) | ((data[5*i+4] >> 6) & 0x0003)) >> 2;
+            pdata[4 * i] = ((((data[5 * i]) << 2) & 0x03FC) | ((data[5 * i + 4] >> 0) & 0x0003)) >> 2;
+            pdata[4 * i + 1] = ((((data[5 * i + 1]) << 2) & 0x03FC) | ((data[5 * i + 4] >> 2) & 0x0003)) >> 2;
+            pdata[4 * i + 2] = ((((data[5 * i + 2]) << 2) & 0x03FC) | ((data[5 * i + 4] >> 4) & 0x0003)) >> 2;
+            pdata[4 * i + 3] = ((((data[5 * i + 3]) << 2) & 0x03FC) | ((data[5 * i + 4] >> 6) & 0x0003)) >> 2;
         }
         /* End - PCIE RAW Data Conversion */
         /* Demosaic */
         // cv::Mat img_byer = cv::Mat(h, w, CV_16UC1, pdata);
         // cv::convertScaleAbs(img_byer, img_byer, 0.25, 0);
         // cv::cvtColor(img_byer, img_rgb8, cv::COLOR_BayerBG2RGB);
-        uchar* buf_rgb = img_rgb8.data;
+        uchar *buf_rgb = img_rgb8.data;
         alg_cv::alg_sdk_cvtColor(pdata, buf_rgb, w, h, alg_cv::ALG_CV_BayerGB2RGB);
         /* Image Display */
-//        cv::resize(img_rgb8, img_rgb8, cv::Size(1280,720));
+        //        cv::resize(img_rgb8, img_rgb8, cv::Size(1280,720));
         cv::namedWindow(image_name, CV_WINDOW_NORMAL);
         cv::imshow(image_name, img_rgb8);
         free(pdata);
     }
-    else if(data_type == ALG_SDK_MIPI_DATA_TYPE_RAW12)
+    else if (data_type == ALG_SDK_MIPI_DATA_TYPE_RAW12)
     {
         /* raw to rgb conversion */
         /* PCIE RAW Data Conversion */
-        uchar* pdata = (uchar*)malloc(sizeof(uchar) * data_size);
+        uchar *pdata = (uchar *)malloc(sizeof(uchar) * data_size);
         for (int i = 0; i < int(data_size / 2); i++)
         {
             pdata[2 * i] = ((((data[3 * i]) << 4) & 0x0FF0) | ((data[3 * i + 2] >> 0) & 0x000F)) >> 4;
@@ -100,12 +101,17 @@ void array_2_mat(uchar* data, int w, int h, int data_type, int ch_id, uint32_t f
         // cv::Mat img_byer = cv::Mat(h, w, CV_16UC1, pdata);
         // cv::convertScaleAbs(img_byer, img_byer, 0.0625, 0);
         // cv::cvtColor(img_byer, img_rgb8, cv::COLOR_BayerBG2RGB);
-        uchar* buf_rgb = img_rgb8.data;
+        uchar *buf_rgb = img_rgb8.data;
         alg_cv::alg_sdk_cvtColor(pdata, buf_rgb, w, h, alg_cv::ALG_CV_BayerGB2RGB);
+
+        cv::Mat img_rgb8_awb = cv::Mat(h, w, CV_8UC3);
+        uchar *buf_rgb_awb = img_rgb8_awb.data;
+        alg_cv::alg_sdk_awb(buf_rgb, buf_rgb_awb, w, h, alg_cv::ALG_CV_AWB_DYM_THRESHOLD);
+
         /* Image Display */
-//        cv::resize(img_rgb8, img_rgb8, cv::Size(1280,720));
+        //        cv::resize(img_rgb8, img_rgb8, cv::Size(1280,720));
         cv::namedWindow(image_name, CV_WINDOW_NORMAL);
-        cv::imshow(image_name, img_rgb8);
+        cv::imshow(image_name, img_rgb8_awb);
         free(pdata);
     }
     /* Image Data Write */
@@ -113,26 +119,25 @@ void array_2_mat(uchar* data, int w, int h, int data_type, int ch_id, uint32_t f
     // if(c == 32)
     // {
     //     /* save raw data */
-        // char filename_2[128] = {};
-        // sprintf(filename_2, "data/image_%02d_%08d.raw", ch_id, frame_index);
-        // ofstream storage_file(filename_2,ios::out | ios::binary);
-        // storage_file.write((char *)data, data_size*2);
-        // storage_file.close();
+    // char filename_2[128] = {};
+    // sprintf(filename_2, "data/image_%02d_%08d.raw", ch_id, frame_index);
+    // ofstream storage_file(filename_2,ios::out | ios::binary);
+    // storage_file.write((char *)data, data_size*2);
+    // storage_file.close();
 
-        /* save image */
-        // char filename[128] = {};
-        // sprintf(filename, "data/image_%02d_%08d.bmp", ch_id, frame_index);
-        // cv::imwrite(filename, img_rgb8);
+    /* save image */
+    // char filename[128] = {};
+    // sprintf(filename, "data/image_%02d_%08d.bmp", ch_id, frame_index);
+    // cv::imwrite(filename, img_rgb8);
     // }
-    
 }
 
 void frame_rate_monitor(const int ch_id, const int frame_index)
 {
-    /* Current frame should be last frame +1, 
+    /* Current frame should be last frame +1,
         otherwise some frames may be lost.
     */
-    if(( (frame_index - g_f_last[ch_id]) <= 1) || (g_f_last[ch_id] == 0) )
+    if (((frame_index - g_f_last[ch_id]) <= 1) || (g_f_last[ch_id] == 0))
     {
         g_f_last[ch_id] = frame_index;
     }
@@ -141,11 +146,11 @@ void frame_rate_monitor(const int ch_id, const int frame_index)
         // printf("Frame Monitor : Frame drop ! [Current Frame %d] [Last Frame %d]\n", frame_index, g_f_last[ch_id]);
         g_f_last[ch_id] = frame_index;
     }
-    
+
     uint64_t t_now = milliseconds();
     g_f_count[ch_id]++;
     uint64_t delta_t = t_now - g_t_last[ch_id];
-    if (delta_t > 1000)  // for 1000 milliseconds
+    if (delta_t > 1000) // for 1000 milliseconds
     {
         g_t_last[ch_id] = t_now;
         printf("Frame Monitor : [Channel %d] [Index %d] [Frame Rate = %f]\n", ch_id, frame_index, (float)g_f_count[ch_id] / delta_t * 1000.0f);
@@ -153,9 +158,9 @@ void frame_rate_monitor(const int ch_id, const int frame_index)
     }
 }
 
-int get_channel_id(const pcie_image_data_t* msg)
+int get_channel_id(const pcie_image_data_t *msg)
 {
-    const char* ptr_topic =  &msg->common_head.topic_name[19];
+    const char *ptr_topic = &msg->common_head.topic_name[19];
     char c_ch_id[2];
     strncpy(c_ch_id, ptr_topic, 2);
     int ch_id = atoi(c_ch_id);
@@ -165,33 +170,33 @@ int get_channel_id(const pcie_image_data_t* msg)
 
 void callback_image_data(void *p)
 {
-    pcie_image_data_t* msg = (pcie_image_data_t*)p;
+    pcie_image_data_t *msg = (pcie_image_data_t *)p;
     /* Debug message */
     int ch_id = get_channel_id(msg);
     printf("[frame = %d], [len %d], [byte_0 = %d], [byte_end = %d]\n",
-           msg->image_info_meta.frame_index,  msg->image_info_meta.img_size, ((uint8_t*)msg->payload)[0], ((uint8_t*)msg->payload)[msg->image_info_meta.img_size - 1]);
+           msg->image_info_meta.frame_index, msg->image_info_meta.img_size, ((uint8_t *)msg->payload)[0], ((uint8_t *)msg->payload)[msg->image_info_meta.img_size - 1]);
 
     /* check frame rate (every 1 second) */
     frame_rate_monitor(ch_id, msg->image_info_meta.frame_index);
 
     /* for Image Display (by OpenCV)
-        This may cause frame rate drop when CPU has run out of resources. 
+        This may cause frame rate drop when CPU has run out of resources.
     */
-   array_2_mat((uchar*)msg->payload, msg->image_info_meta.width, msg->image_info_meta.height, msg->image_info_meta.data_type, ch_id, msg->image_info_meta.frame_index, msg->common_head.topic_name);  // YUV422 type is CV_8U2
+    array_2_mat((uchar *)msg->payload, msg->image_info_meta.width, msg->image_info_meta.height, msg->image_info_meta.data_type, ch_id, msg->image_info_meta.frame_index, msg->common_head.topic_name); // YUV422 type is CV_8U2
 }
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
     signal(SIGINT, int_handler);
 
-    if ((argc == 3) && (strcmp (argv[1], "-c") == 0))
+    if ((argc == 3) && (strcmp(argv[1], "-c") == 0))
     {
         int rc;
-        const char* topic_name = argv[2];
+        const char *topic_name = argv[2];
         printf("subscribe to topic [%s]\n", topic_name);
 
         /* Check the head of topic name */
-        if(strncmp(topic_name, topic_image_head_d, strlen(topic_image_head_d)) == 0)
+        if (strncmp(topic_name, topic_image_head_d, strlen(topic_image_head_d)) == 0)
         {
             rc = alg_sdk_subscribe(topic_name, callback_image_data);
             if (rc < 0)
@@ -206,7 +211,7 @@ int main (int argc, char **argv)
             exit(0);
         }
 
-        if(alg_sdk_init_client())
+        if (alg_sdk_init_client())
         {
             printf("Init Client Error!\n");
             exit(0);
@@ -214,7 +219,7 @@ int main (int argc, char **argv)
         // alg_sdk_stop_client();
         alg_sdk_client_spin_on();
     }
-    else if ((argc == 2) && (strcmp (argv[1], "-all") == 0))
+    else if ((argc == 2) && (strcmp(argv[1], "-all") == 0))
     {
         int rc;
         char image_topic_names[ALG_SDK_MAX_CHANNEL][256];
@@ -231,7 +236,7 @@ int main (int argc, char **argv)
             }
         }
 
-        if(alg_sdk_init_client())
+        if (alg_sdk_init_client())
         {
             printf("Init Client Error!\n");
             exit(0);
